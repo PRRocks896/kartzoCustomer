034caddfb1eca790312f257cfaeb529cea6f7c96
dixitsavaliya-dv <dixit@digitalvichar.in>
2020-10-14 19:18:24 +0530

    [ADD] build changes are done

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var path = require('path');
 
 var app = express();
 
@@ -9,6 +10,79 @@ app.use(express.static(distDir));
 app.use(function(req,res) {
   res.sendFile(path.join(__dirname  + '/build/index.html'));
 });
+
+app.get('/:path', function(req, res) {
+    res.sendFile(path.join(__dirname  + '/build/index.html'));
+});
+
+app.get('/:city/:area', function(req, res) {
+    res.sendFile(path.join(__dirname  + '/build/index.html'));
+});
+
+// app.get('/signin', function(req, res) {
+//     res.sendFile(path.join(__dirname  + '/build/index.html'));
+// });
+
+
+// app.get('/about', function(req, res) {
+//     res.sendFile(path.join(__dirname  + '/build/index.html'));
+// });
+
+// app.get('/contact-us', function(req, res) {
+//     res.sendFile(path.join(__dirname  + '/build/index.html'));
+// });
+
+// app.get('/carrers', function(req, res) {
+//     res.sendFile(path.join(__dirname  + '/build/index.html'));
+// });
+
+
+// app.get('/carrers-job', function(req, res) {
+//     res.sendFile(path.join(__dirname  + '/build/index.html'));
+// });
+
+
+// app.get('/partners', function(req, res) {
+//     res.sendFile(path.join(__dirname  + '/build/index.html'));
+// });
+
+// app.get('/business', function(req, res) {
+//     res.sendFile(path.join(__dirname  + '/build/index.html'));
+// });
+
+// app.get('/search-city', function(req, res) {
+//     res.sendFile(path.join(__dirname  + '/build/index.html'));
+// });
+
+// app.get('/track-order', function(req, res) {
+//     res.sendFile(path.join(__dirname  + '/build/index.html'));
+// });
+
+// app.get('/find-store', function(req, res) {
+//     res.sendFile(path.join(__dirname  + '/build/index.html'));
+// });
+
+// app.get('/store-item', function(req, res) {
+//     res.sendFile(path.join(__dirname  + '/build/index.html'));
+// });
+
+
+app.get('/placeorder', function(req, res) {
+    res.sendFile(path.join(__dirname  + '/build/index.html'));
+});
+
+
+app.get('/cart', function(req, res) {
+    res.sendFile(path.join(__dirname  + '/build/index.html'));
+});
+
+app.get('/profile', function(req, res) {
+    res.sendFile(path.join(__dirname  + '/build/index.html'));
+});
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
